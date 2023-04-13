92af46e973af9ec3f46f3d7628ab51a556baeb4a
DigisCoolisCool <119612928+DigisCoolisCool@users.noreply.github.com>
2023-04-13 14:45:03 +0300

    Versioon1.0

---
+++ DigitalYouth2022/DigitalYouth2022-main/DigitalYouth-main/New Project/Assets/My Game/My Scripts/GameScript.cs
@@ -0,0 +1,52 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+    public EnemySpawner enemySpawner;
+    public int enemiesToSpawn = 10;
+    public int waveToWin = 3;
+    public int waveCount;
+
+
+
+
+    // Use this for initialization
+    protected void Start() { 
+
+        
+
+
+
+
+
+    }
+
+
+    public void Spawnwave()
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            enemySpawner.Spawn();
+        }
+
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        if (EnemySpawner.activated && enemySpawner.transform.childCount == 0)
+        {
+            if (waveCount == 0)
+            {
+
+            }
+            if (waveCount < waveToWin)
+            {
+                waveCount++;
+                HUD.Message("This is round nr" + waveCount);
+                Spawnwave();
+            }
+        }
+
+    }
+}
